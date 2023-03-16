48238f6f2d87f0af2cc391c6159d0ced064f2956
Macolister Bispo <bispo.mac@gmail.com>
2023-03-16 18:31:36 -0400

    commit tree a0f59629dfc8749527c729f1661ce25ed58286cd

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+
+const gifts = [ "teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+
+}
+const cardNames = ["Guadalupe", "Ollie", "Aki"]
+const event = []
+ function writeCards(cardNames, event){
+
+    const writtenCard = [];
+
+    for (let p = 0; p < cardNames.length; p++){
+        writtenCard.push(`Thank you, ${cardNames [p]}, for the wonderful surprise gift!`);
+        
+    }
+    return writtenCard;
+    
+ }
+wrapGifts(gifts);
+
+function countDown(num) {
+
+    while (num >= 0 ){
+        console.log(num);
+        num--;
+    }
+}
+
+
